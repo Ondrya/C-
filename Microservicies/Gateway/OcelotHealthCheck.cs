5ba8b21d03331135b9d27b95ed917751b2914bc2
Andrew <ondrya@yandex.ru>
2020-09-07 20:35:53 +0300

    Create OcelotHealthCheck.cs

---
+++ Microservicies/Gateway/OcelotHealthCheck.cs
@@ -0,0 +1,16 @@
+								// add to pipline
+								var conf = new OcelotPipelineConfiguration()
+                {
+                    PreErrorResponderMiddleware = async (ctx, next) =>
+                    {
+                        if (ctx.HttpContext.Request.Path.Equals(new PathString("/")))
+                        {
+                            await ctx.HttpContext.Response.WriteAsync("ok");
+                        }
+                        else
+                        {
+                            await next.Invoke();
+                        }
+                    }
+                };
+                app.UseOcelot(conf).Wait();
